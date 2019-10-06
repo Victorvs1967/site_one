93d4fb9e968f417eb2b174639951ec904400b91d
User <victorsmirnov@me.com>
2019-10-06 15:08:26 +0300

    edit

--- site_one/blog/models.py
+++ site_one/blog/models.py
@@ -34,5 +34,5 @@ class Post(models.Model):
 
     def get_absolute_url(self):
 
-        return reverse('blog:post_detail', args=[self.published.year, self.published.month, self.publish.day, self.slug])
+        return reverse('blog:post_detail', args=[self.published.year, self.published.month, self.published.day, self.slug])
  
